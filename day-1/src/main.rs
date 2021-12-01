093c9166da8efec5573895c653dca9b728626d6c
Harrison Booth <harrisonbooth@C02G80X2MD6M.local>
2021-12-01 16:25:57 +0000

    Day 1 Part 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,5 +1,3 @@
-use std::fs;
-
 fn count_depth(depths: Vec<i32>) -> i32 {
     let mut depths = depths.iter().peekable();
     let mut count = 0;
@@ -13,17 +11,33 @@ fn count_depth(depths: Vec<i32>) -> i32 {
     count
 }
 
-fn main() {
-    let filename = "input.txt";
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+fn count_depth_groups(depths: Vec<i32>) -> i32 {
+    let mut depth_groups = depths
+        .windows(3)
+        .map(|depth_window| depth_window.iter().sum::<i32>())
+        .peekable();
 
-    let items: Vec<i32> = contents
+    let mut count = 0;
+
+    while let (Some(next), Some(peeked)) = (depth_groups.next(), depth_groups.peek()) {
+        if peeked > &next {
+            count += 1
+        }
+    }
+
+    count
+}
+
+fn main() {
+    let filename = include_str!("../input.txt");
+    let items: Vec<i32> = filename
         .split("\n")
-        .map(|row| row.parse())
-        .filter(|item: &Result<String, _>| item.is_ok())
-        .map(|item| item.unwrap().parse().unwrap())
+        .map(|row| row.parse::<i32>().unwrap_or_else(|err| panic!("{}", err)))
         .collect();
 
-    let count = count_depth(items);
-    println!("{}", count);
+    let count = count_depth(items.clone());
+    println!("Part One: {}", count);
+
+    let count = count_depth_groups(items.clone());
+    println!("Part Two: {}", count);
 }
