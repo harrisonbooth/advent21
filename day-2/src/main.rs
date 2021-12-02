6f19e0b806044a32a42ba94b3a029ee0c17f19fc
Harrison Booth <harrison.booth@and.digital>
2021-12-02 11:47:53 +0000

    Day 2 Both Parts

---
+++ day-2/src/main.rs
@@ -0,0 +1,68 @@
+fn parse_commands(commands: Vec<String>) -> Vec<(String, String)> {
+    commands
+        .iter()
+        .map(|command| command.split_whitespace())
+        .map(|mut command| match (command.next(), command.next()) {
+            (Some(direction), Some(size)) => (direction.to_string(), size.to_string()),
+            _ => panic!("Problem parsing commands."),
+        })
+        .collect()
+}
+
+fn execute_commands(commands: Vec<String>) -> (i32, i32) {
+    let mut horizontal = 0;
+    let mut depth = 0;
+
+    parse_commands(commands)
+        .iter()
+        .for_each(|(direction, size)| {
+            let int_size: i32 = size.parse().unwrap();
+            match direction.as_str() {
+                "forward" => horizontal += int_size,
+                "up" => depth -= int_size,
+                "down" => depth += int_size,
+                direction => panic!("Unrecognised direction {}", direction),
+            }
+        });
+
+    (horizontal, depth)
+}
+
+fn execute_updated_commands(commands: Vec<String>) -> (i32, i32) {
+    let mut aim = 0;
+    let mut horizontal = 0;
+    let mut depth = 0;
+
+    parse_commands(commands)
+        .iter()
+        .for_each(|(direction, size)| {
+            let int_size: i32 = size.parse().unwrap();
+            match direction.as_str() {
+                "forward" => {
+                    horizontal += int_size;
+                    depth += aim * int_size;
+                }
+                "up" => aim -= int_size,
+                "down" => aim += int_size,
+                direction => panic!("Unrecognised direction {}", direction),
+            }
+        });
+
+    (horizontal, depth)
+}
+
+fn main() {
+    let filename = include_str!("../input.txt");
+    let items: Vec<String> = filename
+        .split("\n")
+        .map(|row| row.parse().unwrap_or_else(|err| panic!("{}", err)))
+        .collect();
+
+    let (horizontal, depth) = execute_commands(items.clone());
+    let part_1_product = horizontal * depth;
+    println!("Part One: {}", part_1_product);
+
+    let (horizontal, depth) = execute_updated_commands(items.clone());
+    let part_2_product = horizontal * depth;
+    println!("Part Two: {}", part_2_product);
+}
