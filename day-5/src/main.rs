972c586ce98efdc8f9fb2423f6e0f88fd41877f6
Harrison Booth <harrison.booth@and.digital>
2021-12-05 19:09:47 +0000

    Day 5 Part 2

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -35,10 +35,28 @@ fn main() {
         })
         .collect();
 
-    let mut touched_count: HashMap<Coord, isize> = HashMap::new();
+    let mut cardinal_touched_count: HashMap<Coord, isize> = HashMap::new();
     lines
         .iter()
         .filter(|line| line.is_cardinal())
+        .map(|line| line.touched_coords())
+        .flatten()
+        .for_each(|coord| {
+            cardinal_touched_count
+                .entry(coord)
+                .or_insert(0)
+                .add_assign(1)
+        });
+
+    let multi_cardinal_touched_count = cardinal_touched_count
+        .values()
+        .filter(|&count| count > &1)
+        .collect::<Vec<&isize>>()
+        .len();
+
+    let mut touched_count: HashMap<Coord, isize> = HashMap::new();
+    lines
+        .iter()
         .map(|line| line.touched_coords())
         .flatten()
         .for_each(|coord| touched_count.entry(coord).or_insert(0).add_assign(1));
@@ -49,12 +67,30 @@ fn main() {
         .collect::<Vec<&isize>>()
         .len();
 
-    println!("Total number of points touched: {}", touched_count.len());
+    println!(
+        "Total number of points touched by a cardinal line: {}",
+        cardinal_touched_count.len()
+    );
+
+    println!(
+        "Number of points with more than one crossing cardinal line: {}",
+        multi_cardinal_touched_count,
+    );
+
+    println!(
+        "Total number of points touched by a line: {}",
+        touched_count.len()
+    );
+
+    // println!(
+    //     "Points with more than one crossing line: {:?}",
+    //     touched_count
+    // );
 
     println!(
         "Number of points with more than one crossing line: {}",
-        multi_touched_count,
-    )
+        multi_touched_count
+    );
 }
 
 #[derive(Debug)]
@@ -69,6 +105,46 @@ impl Line {
     }
 
     pub fn touched_coords(&self) -> HashSet<Coord> {
+        match self.is_cardinal() {
+            true => self.cardinal_touched_coords(),
+            false => self.ordinal_touched_coors(),
+        }
+    }
+
+    fn ordinal_touched_coors(&self) -> HashSet<Coord> {
+        let mut touched: HashSet<Coord> = HashSet::new();
+
+        let x_direction = match self.start.x > self.end.x {
+            true => -1,
+            false => 1,
+        };
+
+        let y_direction = match self.start.y > self.end.y {
+            true => -1,
+            false => 1,
+        };
+
+        let mut current_x = self.start.x;
+        let mut current_y = self.start.y;
+
+        let initial_coord = Coord::new(current_x, current_y);
+        touched.insert(initial_coord);
+
+        loop {
+            current_x += x_direction;
+            current_y += y_direction;
+            let touched_coord = Coord::new(current_x, current_y);
+            touched.insert(touched_coord);
+
+            if current_x == self.end.x && current_y == self.end.y {
+                break;
+            }
+        }
+
+        touched
+    }
+
+    fn cardinal_touched_coords(&self) -> HashSet<Coord> {
         let mut touched: HashSet<Coord> = HashSet::new();
 
         let x_range = match self.start.x > self.end.x {
