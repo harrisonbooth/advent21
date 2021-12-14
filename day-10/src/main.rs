3e3a54506d1bee3417299332aca9cdefcf6ba6f5
Harrison Booth <harrison.booth@and.digital>
2021-12-14 11:22:01 +0000

    Day 10 Part 2 (Messy)

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -31,6 +31,42 @@ pub fn main() {
         .collect();
 
     println!("Part one: {}", total_illegal_chars(&illegal_chars));
+
+    let incomplete_lines: Vec<Vec<&str>> = lines
+        .into_iter()
+        .filter(|line| line_checker.check_line(line).is_none())
+        .collect();
+
+    let added_closer_sets: Vec<Vec<String>> = incomplete_lines
+        .iter()
+        .map(|line| line_checker.complete_line(line))
+        .collect();
+
+    let mut added_closer_scores: Vec<usize> = added_closer_sets
+        .iter()
+        .map(|closer_set| total_added_closers(closer_set))
+        .collect();
+
+    added_closer_scores.sort();
+
+    println!(
+        "Part Two: {:?}",
+        added_closer_scores[added_closer_scores.len() / 2]
+    )
+}
+
+fn total_added_closers(closer_set: &Vec<String>) -> usize {
+    closer_set.iter().fold(0, |acc, closer| {
+        let addition = match closer.as_str() {
+            ")" => 1,
+            "]" => 2,
+            "}" => 3,
+            ">" => 4,
+            _ => panic!("Illegal closer: {}", closer),
+        };
+
+        (acc * 5) + addition
+    })
 }
 
 pub fn total_illegal_chars(illegal_chars: &Vec<String>) -> usize {
@@ -79,5 +115,32 @@ impl<'a> LineChecker<'a> {
             }
         }
         None
+    }
+
+    pub fn complete_line(&'a self, line: &Vec<&str>) -> Vec<String> {
+        let mut char_stack: Vec<&str> = vec![];
+
+        for character in line.iter() {
+            match self
+                .charset
+                .keys()
+                .collect::<Vec<&&str>>()
+                .contains(&character)
+            {
+                true => char_stack.push(character),
+                false => {
+                    let most_recent_character = char_stack.last().unwrap();
+                    if self.charset.get(most_recent_character).unwrap() == character {
+                        char_stack.pop();
+                    }
+                }
+            }
+        }
+
+        char_stack.reverse();
+        char_stack
+            .iter()
+            .map(|character| self.charset.get(character).unwrap().to_string())
+            .collect()
     }
 }
