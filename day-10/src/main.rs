384bcf46cda82b3995f4deb6c6aaac12ffffb2b1
Harrison Booth <harrison.booth@and.digital>
2021-12-14 11:34:47 +0000

    Day 10 Part 2 Abstract some logic to a FileChecker struct

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -15,77 +15,83 @@ pub fn main() {
         })
         .collect();
 
-    let mut part_one_charset = HashMap::new();
-    part_one_charset.insert("(", ")");
-    part_one_charset.insert("[", "]");
-    part_one_charset.insert("{", "}");
-    part_one_charset.insert("<", ">");
+    let mut charset = HashMap::new();
+    charset.insert("(", ")");
+    charset.insert("[", "]");
+    charset.insert("{", "}");
+    charset.insert("<", ">");
 
-    let line_checker = LineChecker {
-        charset: part_one_charset,
-    };
+    let file_checker = FileChecker::new(charset);
 
-    let illegal_chars: Vec<String> = lines
-        .iter()
-        .filter_map(|line| line_checker.check_line(line))
-        .collect();
-
-    println!("Part one: {}", total_illegal_chars(&illegal_chars));
-
-    let incomplete_lines: Vec<Vec<&str>> = lines
-        .into_iter()
-        .filter(|line| line_checker.check_line(line).is_none())
-        .collect();
-
-    let added_closer_sets: Vec<Vec<String>> = incomplete_lines
-        .iter()
-        .map(|line| line_checker.complete_line(line))
-        .collect();
-
-    let mut added_closer_scores: Vec<usize> = added_closer_sets
-        .iter()
-        .map(|closer_set| total_added_closers(closer_set))
-        .collect();
-
-    added_closer_scores.sort();
+    println!(
+        "Part one: {:?}",
+        file_checker.total_illegal_characters(&lines)
+    );
 
     println!(
         "Part Two: {:?}",
+        file_checker.total_autocompleted_characters(&lines)
+    );
+}
+
+struct FileChecker<'a> {
+    line_checker: LineChecker<'a>,
+}
+
+impl<'a> FileChecker<'a> {
+    pub fn new(charset: HashMap<&'a str, &'a str>) -> Self {
+        Self {
+            line_checker: LineChecker { charset },
+        }
+    }
+
+    pub fn total_illegal_characters(&self, lines: &Vec<Vec<&'a str>>) -> usize {
+        let illegal_chars: Vec<String> = lines
+            .iter()
+            .filter_map(|line| self.line_checker.check_line(line))
+            .collect();
+
+        let mut character_counts: HashMap<&String, usize> = HashMap::new();
+        illegal_chars
+            .iter()
+            .for_each(|character| character_counts.entry(character).or_insert(0).add_assign(1));
+
+        character_counts.iter().fold(0, |acc, (key, value)| {
+            let multiplier = match key.as_str() {
+                ")" => 3,
+                "]" => 57,
+                "}" => 1197,
+                ">" => 25137,
+                _ => panic!("Illegal illegal character: {}", value),
+            };
+
+            acc + (multiplier * value)
+        })
+    }
+
+    pub fn total_autocompleted_characters(&self, lines: &Vec<Vec<&'a str>>) -> usize {
+        let mut added_closer_scores: Vec<usize> = lines
+            .into_iter()
+            .filter(|line| self.line_checker.check_line(line).is_none())
+            .map(|line| self.line_checker.complete_line(&line))
+            .map(|closer_set| {
+                closer_set.iter().fold(0, |acc, closer| {
+                    let addition = match closer.as_str() {
+                        ")" => 1,
+                        "]" => 2,
+                        "}" => 3,
+                        ">" => 4,
+                        _ => panic!("Illegal closer: {}", closer),
+                    };
+
+                    (acc * 5) + addition
+                })
+            })
+            .collect();
+
+        added_closer_scores.sort();
         added_closer_scores[added_closer_scores.len() / 2]
-    )
-}
-
-fn total_added_closers(closer_set: &Vec<String>) -> usize {
-    closer_set.iter().fold(0, |acc, closer| {
-        let addition = match closer.as_str() {
-            ")" => 1,
-            "]" => 2,
-            "}" => 3,
-            ">" => 4,
-            _ => panic!("Illegal closer: {}", closer),
-        };
-
-        (acc * 5) + addition
-    })
-}
-
-pub fn total_illegal_chars(illegal_chars: &Vec<String>) -> usize {
-    let mut character_counts: HashMap<&String, usize> = HashMap::new();
-    illegal_chars
-        .iter()
-        .for_each(|character| character_counts.entry(character).or_insert(0).add_assign(1));
-
-    character_counts.iter().fold(0, |acc, (key, value)| {
-        let multiplier = match key.as_str() {
-            ")" => 3,
-            "]" => 57,
-            "}" => 1197,
-            ">" => 25137,
-            _ => panic!("Illegal illegal character: {}", value),
-        };
-
-        acc + (multiplier * value)
-    })
+    }
 }
 
 struct LineChecker<'a> {
